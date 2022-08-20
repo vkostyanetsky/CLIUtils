3ef392608f49e55a0c724456127a7c92ed3d38e2
Vlad Kostyanetsky <vlad@kostyanetsky.ru>
2022-08-20 18:29:17 +0700

    Pleased black

--- vkostyanetsky/cliutils/__init__.py
+++ vkostyanetsky/cliutils/__init__.py
@@ -6,4 +6,11 @@ from vkostyanetsky.cliutils.prompt import ask_for_enter
 from vkostyanetsky.cliutils.prompt import ask_for_yes_or_no
 from vkostyanetsky.cliutils.prompt import title_and_value
 
-__all__ = ["Menu", "MenuBorders", "clear_terminal", "ask_for_enter", "ask_for_yes_or_no", "title_and_value"]
+__all__ = [
+    "Menu",
+    "MenuBorders",
+    "clear_terminal",
+    "ask_for_enter",
+    "ask_for_yes_or_no",
+    "title_and_value",
+]
